ef0d766e6d699c9e56e490d6dbce4728ce588321
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-07-12 15:39:43 +0200

    chore(__tests__/index.ts) file created

---
+++ __tests__/unit/index.ts
@@ -0,0 +1,19 @@
+import {
+  // TODO: import your class and types there
+} from "../../src";
+
+describe("Your context", () => {
+  it("Should work", async () => {
+    try {
+    } catch (e) {
+    }
+  });
+
+  it("Should not work", async () => {
+    try {
+
+    } catch (e) {
+
+    }
+  });
+});
