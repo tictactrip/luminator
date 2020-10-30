1f05a54fcf4391c85e9123106a971ef31e59ffd2
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2020-10-30 12:05:26 +0100

    tests(__tests__/unit/index.ts): "country" param become "countries"

--- __tests__/unit/index.ts
+++ __tests__/unit/index.ts
@@ -15,7 +15,7 @@ describe('Luminator', () => {
       luminator = new Luminator({ luminatiConfig });
     });
 
-    it('should create an agent with a random country and sessionId', async () => {
+    it('should create an agent with a random countries and sessionId', async () => {
       luminator.changeIp();
 
       expect(luminator.axios.defaults.httpsAgent.options.host).toBe('zproxy.lum-superproxy.io');
@@ -24,8 +24,8 @@ describe('Luminator', () => {
       expect(luminator.axios.defaults.httpsAgent.options.auth).toMatch(new RegExp(`${luminatiConfig.zone}-session-([0-9])*-country-(${countryKeys.join('|')}):${luminatiConfig.password}`));
     });
 
-    it('should create an agent with a specific country and a random sessionId', async () => {
-      luminator.changeIp({country: ELuminatiCountry.FRANCE});
+    it('should create an agent with a specific countries and a random sessionId', async () => {
+      luminator.changeIp({countries: [ELuminatiCountry.FRANCE]});
 
       expect(luminator.axios.defaults.httpsAgent.options.host).toBe('zproxy.lum-superproxy.io');
       expect(luminator.axios.defaults.httpsAgent.options.port).toBe(22225);
@@ -33,10 +33,10 @@ describe('Luminator', () => {
       expect(luminator.axios.defaults.httpsAgent.options.auth).toMatch(new RegExp(`${luminatiConfig.zone}-session-([0-9])*-country-fr:${luminatiConfig.password}`));
     });
 
-    it('should create an agent with a specific country and a specific sessionId', async () => {
+    it('should create an agent with a specific countries and a specific sessionId', async () => {
       const sessionId: number = 123456789;
 
-      luminator.changeIp({country: ELuminatiCountry.FRANCE, sessionId });
+      luminator.changeIp({countries: [ELuminatiCountry.FRANCE], sessionId });
 
       expect(luminator.axios.defaults.httpsAgent.options.host).toBe('zproxy.lum-superproxy.io');
       expect(luminator.axios.defaults.httpsAgent.options.port).toBe(22225);
@@ -44,7 +44,7 @@ describe('Luminator', () => {
       expect(luminator.axios.defaults.httpsAgent.options.auth).toMatch(new RegExp(`${luminatiConfig.zone}-session-${sessionId}-country-fr:${luminatiConfig.password}`));
     });
 
-    it('should create an agent with a random country and a specific sessionId', async () => {
+    it('should create an agent with a random countries and a specific sessionId', async () => {
       const sessionId: number = 123456789;
 
       luminator.changeIp({ sessionId });
