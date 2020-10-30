41e50c3f5023d0ff286c79083ef5217b1d27acfe
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2020-10-30 14:47:49 +0100

    style(__tests__/unit/index.ts): lint:fix

--- __tests__/unit/index.ts
+++ __tests__/unit/index.ts
@@ -4,8 +4,8 @@ describe('Luminator', () => {
   // tslint:disable-next-line:mocha-no-side-effect-code
   const countryKeys: string[] = Object.values(ELuminatiCountry);
   const luminatiConfig = {
-    zone: "luminati-tictactrip",
-    password: "nf8fxes1oub"
+    zone: 'luminati-tictactrip',
+    password: 'nf8fxes1oub',
   };
 
   describe('#changeIp', () => {
@@ -21,38 +21,50 @@ describe('Luminator', () => {
       expect(luminator.axios.defaults.httpsAgent.options.host).toBe('zproxy.lum-superproxy.io');
       expect(luminator.axios.defaults.httpsAgent.options.port).toBe(22225);
       expect(luminator.axios.defaults.httpsAgent.options.rejectUnauthorized).toBe(false);
-      expect(luminator.axios.defaults.httpsAgent.options.auth).toMatch(new RegExp(`${luminatiConfig.zone}-session-([0-9])*-country-(${countryKeys.join('|')}):${luminatiConfig.password}`));
+      expect(luminator.axios.defaults.httpsAgent.options.auth).toMatch(
+        new RegExp(
+          `${luminatiConfig.zone}-session-([0-9])*-country-(${countryKeys.join('|')}):${luminatiConfig.password}`,
+        ),
+      );
     });
 
     it('should create an agent with a specific countries and a random sessionId', async () => {
-      luminator.changeIp({countries: [ELuminatiCountry.FRANCE]});
+      luminator.changeIp({ countries: [ELuminatiCountry.FRANCE] });
 
       expect(luminator.axios.defaults.httpsAgent.options.host).toBe('zproxy.lum-superproxy.io');
       expect(luminator.axios.defaults.httpsAgent.options.port).toBe(22225);
       expect(luminator.axios.defaults.httpsAgent.options.rejectUnauthorized).toBe(false);
-      expect(luminator.axios.defaults.httpsAgent.options.auth).toMatch(new RegExp(`${luminatiConfig.zone}-session-([0-9])*-country-fr:${luminatiConfig.password}`));
+      expect(luminator.axios.defaults.httpsAgent.options.auth).toMatch(
+        new RegExp(`${luminatiConfig.zone}-session-([0-9])*-country-fr:${luminatiConfig.password}`),
+      );
     });
 
     it('should create an agent with a specific countries and a specific sessionId', async () => {
-      const sessionId: number = 123456789;
+      const sessionId = 123456789;
 
-      luminator.changeIp({countries: [ELuminatiCountry.FRANCE], sessionId });
+      luminator.changeIp({ countries: [ELuminatiCountry.FRANCE], sessionId });
 
       expect(luminator.axios.defaults.httpsAgent.options.host).toBe('zproxy.lum-superproxy.io');
       expect(luminator.axios.defaults.httpsAgent.options.port).toBe(22225);
       expect(luminator.axios.defaults.httpsAgent.options.rejectUnauthorized).toBe(false);
-      expect(luminator.axios.defaults.httpsAgent.options.auth).toMatch(new RegExp(`${luminatiConfig.zone}-session-${sessionId}-country-fr:${luminatiConfig.password}`));
+      expect(luminator.axios.defaults.httpsAgent.options.auth).toMatch(
+        new RegExp(`${luminatiConfig.zone}-session-${sessionId}-country-fr:${luminatiConfig.password}`),
+      );
     });
 
     it('should create an agent with a random countries and a specific sessionId', async () => {
-      const sessionId: number = 123456789;
+      const sessionId = 123456789;
 
       luminator.changeIp({ sessionId });
 
       expect(luminator.axios.defaults.httpsAgent.options.host).toBe('zproxy.lum-superproxy.io');
       expect(luminator.axios.defaults.httpsAgent.options.port).toBe(22225);
       expect(luminator.axios.defaults.httpsAgent.options.rejectUnauthorized).toBe(false);
-      expect(luminator.axios.defaults.httpsAgent.options.auth).toMatch(new RegExp(`${luminatiConfig.zone}-session-${sessionId}-country-(${countryKeys.join('|')}):${luminatiConfig.password}`));
+      expect(luminator.axios.defaults.httpsAgent.options.auth).toMatch(
+        new RegExp(
+          `${luminatiConfig.zone}-session-${sessionId}-country-(${countryKeys.join('|')}):${luminatiConfig.password}`,
+        ),
+      );
     });
   });
 });
