3d7d35f900c2af44e6bd1992ebe689c5dbfaf53f
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2020-11-09 10:32:12 +0000

    tests(__tests__/unit/index.ts): test case created

--- __tests__/unit/index.ts
+++ __tests__/unit/index.ts
@@ -1,11 +1,12 @@
-import { Luminator, ELuminatiCountry } from '../../src';
+import * as nock from 'nock';
+import { ELuminatiCountry, EStrategyMode, Luminator } from '../../src';
 
 describe('Luminator', () => {
   // tslint:disable-next-line:mocha-no-side-effect-code
   const countryKeys: string[] = Object.values(ELuminatiCountry);
   const luminatiConfig = {
-    zone: 'luminati-tictactrip',
-    password: 'nf8fxes1oub',
+    zone: 'lum-customer-tictactrip-zone-luminatordevtoremove',
+    password: 'pdit81p1l7jt',
   };
 
   describe('#changeIp', () => {
@@ -65,6 +66,96 @@ describe('Luminator', () => {
           `${luminatiConfig.zone}-session-${sessionId}-country-(${countryKeys.join('|')}):${luminatiConfig.password}`,
         ),
       );
+    });
+  });
+
+  describe('#strategy', function () {
+    it('should change ip every requests', async () => {
+      const luminator: Luminator = new Luminator({
+        luminatiConfig,
+        strategy: {
+          mode: EStrategyMode.CHANGE_IP_EVERY_REQUESTS,
+          countries: [ELuminatiCountry.FRANCE, ELuminatiCountry.SPAIN],
+        },
+      });
+
+      const response1 = {
+        ip: '184.174.62.231',
+        country: 'FR',
+        asn: {
+          asnum: 9009,
+          org_name: 'M247 Ltd',
+        },
+        geo: {
+          city: 'Paris',
+          region: 'IDF',
+          region_name: 'Île-de-France',
+          postal_code: '75014',
+          latitude: 48.8579,
+          longitude: 2.3491,
+          tz: 'Europe/Paris',
+          lum_city: 'paris',
+          lum_region: 'idf',
+        },
+      };
+
+      const response2 = {
+        ip: '178.171.89.101',
+        country: 'ES',
+        asn: {
+          asnum: 9009,
+          org_name: 'M247 Ltd',
+        },
+        geo: {
+          city: 'Madrid',
+          region: 'MD',
+          region_name: 'Madrid',
+          postal_code: '28001',
+          latitude: 40.4167,
+          longitude: -3.6838,
+          tz: 'Europe/Madrid',
+          lum_city: 'madrid',
+          lum_region: 'md',
+        },
+      };
+
+      const request1Mock = nock('https://lumtest.com').get('/myip.json').once().reply(200, response1);
+      const request2Mock = nock('https://lumtest.com').get('/myip.json').once().reply(200, response2);
+
+      const result1 = await luminator.fetch({
+        method: 'get',
+        baseURL: 'https://lumtest.com',
+        url: '/myip.json',
+      });
+
+      const result2 = await luminator.fetch({
+        method: 'get',
+        baseURL: 'https://lumtest.com',
+        url: '/myip.json',
+      });
+
+      expect(result1.data).toStrictEqual(response1);
+      expect(result2.data).toStrictEqual(response2);
+
+      request1Mock.done();
+      request2Mock.done();
+    });
+
+    it('should throw an error if no countries have been given into the constructor', async () => {
+      let error: Error;
+      try {
+        new Luminator({
+          luminatiConfig,
+          strategy: {
+            mode: EStrategyMode.CHANGE_IP_EVERY_REQUESTS,
+            countries: [],
+          },
+        });
+      } catch (e) {
+        error = e;
+      }
+
+      expect(error).toStrictEqual(new Error('"countries" array cannot be empty'));
     });
   });
 });
