4165b376a126531b25d57f2954a03c894cb61688
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2020-11-09 14:22:24 +0000

    tests(__tests__/unit/index.ts): test sessionId and country class values

--- __tests__/unit/index.ts
+++ __tests__/unit/index.ts
@@ -27,7 +27,7 @@ describe('Luminator', () => {
     });
 
     it('should set an axios default configuration (User-Agent header)', async () => {
-      const userAgent =
+      const userAgent: string =
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36';
       const response1 = {
         ip: '216.74.105.107',
@@ -79,7 +79,9 @@ describe('Luminator', () => {
   describe('#changeIp', () => {
     let luminator: Luminator;
 
-    beforeAll(() => {
+    const regexPatternAllCountries: string = `${countryKeys.join('|')}`;
+
+    beforeEach(() => {
       luminator = new Luminator({ luminatiConfig });
     });
 
@@ -87,12 +89,14 @@ describe('Luminator', () => {
       const response: Luminator = await luminator.changeIp();
 
       expect(response).toBeInstanceOf(Luminator);
+      expect(typeof luminator.sessionId).toEqual('number');
+      expect(luminator.country).toMatch(new RegExp(regexPatternAllCountries));
       expect(luminator.axios.defaults.httpsAgent.proxy.host).toBe('zproxy.lum-superproxy.io');
       expect(luminator.axios.defaults.httpsAgent.proxy.port).toBe(22225);
       expect(luminator.axios.defaults.httpsAgent.proxy.rejectUnauthorized).toBe(false);
       expect(luminator.axios.defaults.httpsAgent.proxy.auth).toMatch(
         new RegExp(
-          `${luminatiConfig.zone}-session-([0-9])*-country-(${countryKeys.join('|')}):${luminatiConfig.password}`,
+          `${luminatiConfig.zone}-session-([0-9])*-country-(${regexPatternAllCountries}):${luminatiConfig.password}`,
         ),
       );
     });
@@ -101,6 +105,8 @@ describe('Luminator', () => {
       const response: Luminator = luminator.changeIp({ countries: [ELuminatiCountry.FRANCE] });
 
       expect(response).toBeInstanceOf(Luminator);
+      expect(typeof luminator.sessionId).toEqual('number');
+      expect(luminator.country).toMatch(/fr/);
       expect(luminator.axios.defaults.httpsAgent.proxy.host).toBe('zproxy.lum-superproxy.io');
       expect(luminator.axios.defaults.httpsAgent.proxy.port).toBe(22225);
       expect(luminator.axios.defaults.httpsAgent.proxy.rejectUnauthorized).toBe(false);
@@ -115,6 +121,8 @@ describe('Luminator', () => {
       const response: Luminator = luminator.changeIp({ countries: [ELuminatiCountry.FRANCE], sessionId });
 
       expect(response).toBeInstanceOf(Luminator);
+      expect(luminator.sessionId).toEqual(123456789);
+      expect(luminator.country).toMatch(/fr/);
       expect(luminator.axios.defaults.httpsAgent.proxy.host).toBe('zproxy.lum-superproxy.io');
       expect(luminator.axios.defaults.httpsAgent.proxy.port).toBe(22225);
       expect(luminator.axios.defaults.httpsAgent.proxy.rejectUnauthorized).toBe(false);
@@ -129,12 +137,14 @@ describe('Luminator', () => {
       const response: Luminator = luminator.changeIp({ sessionId });
 
       expect(response).toBeInstanceOf(Luminator);
+      expect(luminator.sessionId).toEqual(123456789);
+      expect(luminator.country).toMatch(new RegExp(regexPatternAllCountries));
       expect(luminator.axios.defaults.httpsAgent.proxy.host).toBe('zproxy.lum-superproxy.io');
       expect(luminator.axios.defaults.httpsAgent.proxy.port).toBe(22225);
       expect(luminator.axios.defaults.httpsAgent.proxy.rejectUnauthorized).toBe(false);
       expect(luminator.axios.defaults.httpsAgent.proxy.auth).toMatch(
         new RegExp(
-          `${luminatiConfig.zone}-session-${sessionId}-country-(${countryKeys.join('|')}):${luminatiConfig.password}`,
+          `${luminatiConfig.zone}-session-${sessionId}-country-(${regexPatternAllCountries}):${luminatiConfig.password}`,
         ),
       );
     });
@@ -160,152 +170,127 @@ describe('Luminator', () => {
 
       expect(error).toStrictEqual(new Error('"countries" array cannot be empty'));
     });
+
+    it('should throw an error if fetch() has been called without previously used changeIp() or set a strategy', async () => {
+      let error: Error;
+      try {
+        await luminator.fetch({
+          method: 'get',
+          baseURL: 'https://lumtest.com',
+          url: '/myip.json',
+        });
+      } catch (e) {
+        error = e;
+      }
+
+      expect(error).toStrictEqual(
+        new Error('Your are trying to send a request without setting a Strategy or calling changeIp().'),
+      );
+    });
   });
 
   describe('#strategy', () => {
-    describe('CHANGE_IP_EVERY_REQUESTS (HTTP)', () => {
-      it('should change ip every requests', async () => {
-        const luminator: Luminator = new Luminator({
-          luminatiConfig,
-          strategy: {
-            mode: EStrategyMode.CHANGE_IP_EVERY_REQUESTS,
-            countries: [ELuminatiCountry.FRANCE, ELuminatiCountry.SPAIN],
-          },
-        });
-
-        const response1 = {
-          ip: '184.174.62.231',
-          country: 'FR',
-          asn: {
-            asnum: 9009,
-            org_name: 'M247 Ltd',
-          },
-          geo: {
-            city: 'Paris',
-            region: 'IDF',
-            region_name: 'Île-de-France',
-            postal_code: '75014',
-            latitude: 48.8579,
-            longitude: 2.3491,
-            tz: 'Europe/Paris',
-            lum_city: 'paris',
-            lum_region: 'idf',
-          },
-        };
-
-        const response2 = {
-          ip: '178.171.89.101',
-          country: 'ES',
-          asn: {
-            asnum: 9009,
-            org_name: 'M247 Ltd',
-          },
-          geo: {
-            city: 'Madrid',
-            region: 'MD',
-            region_name: 'Madrid',
-            postal_code: '28001',
-            latitude: 40.4167,
-            longitude: -3.6838,
-            tz: 'Europe/Madrid',
-            lum_city: 'madrid',
-            lum_region: 'md',
-          },
-        };
-
-        const request1Mock = nock('https://lumtest.com').get('/myip.json').once().reply(200, response1);
-        const request2Mock = nock('https://lumtest.com').get('/myip.json').once().reply(200, response2);
-
-        const result1 = await luminator.fetch({
-          method: 'get',
-          baseURL: 'https://lumtest.com',
-          url: '/myip.json',
-        });
-
-        const result2 = await luminator.fetch({
-          method: 'get',
-          baseURL: 'https://lumtest.com',
-          url: '/myip.json',
-        });
-
-        expect(result1.data).toStrictEqual(response1);
-        expect(result2.data).toStrictEqual(response2);
-
-        request1Mock.done();
-        request2Mock.done();
+    describe('CHANGE_IP_EVERY_REQUESTS', () => {
+      const luminator: Luminator = new Luminator({
+        luminatiConfig,
+        strategy: {
+          mode: EStrategyMode.CHANGE_IP_EVERY_REQUESTS,
+          countries: [ELuminatiCountry.FRANCE, ELuminatiCountry.SPAIN],
+        },
       });
-    });
 
-    describe('CHANGE_IP_EVERY_REQUESTS (HTTPS)', () => {
-      it('should change ip every requests', async () => {
-        const luminator: Luminator = new Luminator({
-          luminatiConfig,
-          strategy: {
-            mode: EStrategyMode.CHANGE_IP_EVERY_REQUESTS,
-            countries: [ELuminatiCountry.FRANCE, ELuminatiCountry.SPAIN],
-          },
+      const response1 = {
+        ip: '184.174.62.231',
+        country: 'FR',
+        asn: {
+          asnum: 9009,
+          org_name: 'M247 Ltd',
+        },
+        geo: {
+          city: 'Paris',
+          region: 'IDF',
+          region_name: 'Île-de-France',
+          postal_code: '75014',
+          latitude: 48.8579,
+          longitude: 2.3491,
+          tz: 'Europe/Paris',
+          lum_city: 'paris',
+          lum_region: 'idf',
+        },
+      };
+
+      const response2 = {
+        ip: '178.171.89.101',
+        country: 'ES',
+        asn: {
+          asnum: 9009,
+          org_name: 'M247 Ltd',
+        },
+        geo: {
+          city: 'Madrid',
+          region: 'MD',
+          region_name: 'Madrid',
+          postal_code: '28001',
+          latitude: 40.4167,
+          longitude: -3.6838,
+          tz: 'Europe/Madrid',
+          lum_city: 'madrid',
+          lum_region: 'md',
+        },
+      };
+
+      describe('http', () => {
+        it('should change ip every request', async () => {
+          const request1Mock = nock('http://lumtest.com').get('/myip.json').once().reply(200, response1);
+          const request2Mock = nock('http://lumtest.com').get('/myip.json').once().reply(200, response2);
+
+          const result1 = await luminator.fetch({
+            method: 'get',
+            baseURL: 'http://lumtest.com',
+            url: '/myip.json',
+          });
+
+          const result2 = await luminator.fetch({
+            method: 'get',
+            baseURL: 'http://lumtest.com',
+            url: '/myip.json',
+          });
+
+          expect(typeof luminator.sessionId).toEqual('number');
+          expect(luminator.country).toMatch(/fr|es/);
+          expect(result1.data).toStrictEqual(response1);
+          expect(result2.data).toStrictEqual(response2);
+
+          request1Mock.done();
+          request2Mock.done();
         });
+      });
 
-        const response1 = {
-          ip: '184.174.62.231',
-          country: 'FR',
-          asn: {
-            asnum: 9009,
-            org_name: 'M247 Ltd',
-          },
-          geo: {
-            city: 'Paris',
-            region: 'IDF',
-            region_name: 'Île-de-France',
-            postal_code: '75014',
-            latitude: 48.8579,
-            longitude: 2.3491,
-            tz: 'Europe/Paris',
-            lum_city: 'paris',
-            lum_region: 'idf',
-          },
-        };
+      describe('https', () => {
+        it('should change ip every request', async () => {
+          const request1Mock = nock('https://lumtest.com').get('/myip.json').once().reply(200, response1);
+          const request2Mock = nock('https://lumtest.com').get('/myip.json').once().reply(200, response2);
 
-        const response2 = {
-          ip: '178.171.89.101',
-          country: 'ES',
-          asn: {
-            asnum: 9009,
-            org_name: 'M247 Ltd',
-          },
-          geo: {
-            city: 'Madrid',
-            region: 'MD',
-            region_name: 'Madrid',
-            postal_code: '28001',
-            latitude: 40.4167,
-            longitude: -3.6838,
-            tz: 'Europe/Madrid',
-            lum_city: 'madrid',
-            lum_region: 'md',
-          },
-        };
+          const result1 = await luminator.fetch({
+            method: 'get',
+            baseURL: 'https://lumtest.com',
+            url: '/myip.json',
+          });
 
-        const request1Mock = nock('http://lumtest.com').get('/myip.json').once().reply(200, response1);
-        const request2Mock = nock('http://lumtest.com').get('/myip.json').once().reply(200, response2);
+          const result2 = await luminator.fetch({
+            method: 'get',
+            baseURL: 'https://lumtest.com',
+            url: '/myip.json',
+          });
 
-        const result1 = await luminator.fetch({
-          method: 'get',
-          baseURL: 'http://lumtest.com',
-          url: '/myip.json',
+          expect(typeof luminator.sessionId).toEqual('number');
+          expect(luminator.country).toMatch(/fr|es/);
+          expect(result1.data).toStrictEqual(response1);
+          expect(result2.data).toStrictEqual(response2);
+
+          request1Mock.done();
+          request2Mock.done();
         });
-
-        const result2 = await luminator.fetch({
-          method: 'get',
-          baseURL: 'http://lumtest.com',
-          url: '/myip.json',
-        });
-
-        expect(result1.data).toStrictEqual(response1);
-        expect(result2.data).toStrictEqual(response2);
-
-        request1Mock.done();
-        request2Mock.done();
       });
     });
   });
