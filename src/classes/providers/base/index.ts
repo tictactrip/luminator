4defcff3fd1e96cc5c46473ac656a72cec4c88f5
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2020-12-03 16:37:11 +0100

    feat(classes/providers/base/index.ts): contract updated

--- src/classes/providers/base/index.ts
+++ src/classes/providers/base/index.ts
@@ -1,10 +1,44 @@
-import { ICreateProxyConfig, ICreateProxy } from './types';
+import {ICreateProxyConfig, ICreateProxy, ECountry} from './types';
 
 export abstract class Provider {
+
   /**
-   * @description Create https and http proxies.
+   * @description Returns session id.
+   * @returns {number}
+   */
+  public abstract getSessionId(): number;
+
+  /**
+   * @description Returns country.
+   * @returns {ECountry}
+   */
+  public abstract getCountry(): ECountry;
+
+  /**
+   * @description Creates proxy agents with a random country and sessionId.
    * @param {ICreateProxy} params
    * @returns {ICreateProxyConfig}
    */
-  public abstract createProxyAgents(params: ICreateProxy): ICreateProxyConfig;
+  public abstract createAgentsWithRandomCountryAndSessionId(params: ICreateProxy): ICreateProxyConfig;
+
+  /**
+   * @description Creates proxy agents with specific countries and a specific sessionId.
+   * @param {ICreateProxy} params
+   * @returns {ICreateProxyConfig}
+   */
+  public abstract createAgentsSpecificCountryAndSessionsId(params: ICreateProxy): ICreateProxyConfig;
+
+  /**
+   * @description Create proxy agents with a specific country and a random sessionId.
+   * @param {ICreateProxy} params
+   * @returns {ICreateProxyConfig}
+   */
+  public abstract createAgentsSpecificCountriesAndRandomSessionId(params: ICreateProxy): ICreateProxyConfig;
+
+  /**
+   * @description Creates proxy agents with a random country and a specific sessionId
+   * @param {ICreateProxy} params
+   * @returns {ICreateProxyConfig}
+   */
+  public abstract createAgentsRandomCountryAndSpecificSessionId(params: ICreateProxy): ICreateProxyConfig;
 }
