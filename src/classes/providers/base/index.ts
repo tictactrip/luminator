f6854337d114649843cda6b46f559f68390c9b34
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2020-12-07 17:09:54 +0100

    feat(classes/providers/base/index.ts): file created

--- src/classes/providers/base/index.ts
+++ src/classes/providers/base/index.ts
@@ -1,43 +1,28 @@
-import { ICreateProxyConfig, ICreateProxy, ECountry } from './types';
+import {AxiosInstance, AxiosPromise, AxiosRequestConfig} from "axios";
 
-export abstract class Provider {
+export class Base {
+  public axios: AxiosInstance;
+
   /**
-   * @description Returns session id.
+   * @description Sends request.
+   * @param {AxiosRequestConfig} axiosRequestConfig
+   * @returns {AxiosPromise}
+   */
+  fetch(axiosRequestConfig: AxiosRequestConfig): AxiosPromise {
+    if (!this.axios.defaults.httpsAgent && !this.axios.defaults.httpAgent) {
+      throw new Error('Your are trying to send a request without setting a Strategy or calling setIp().');
+    }
+
+    return this.axios(axiosRequestConfig);
+  }
+
+  /**
+   * @returns Returns a number between two included numbers.
+   * @param {number} min
+   * @param {number} max
    * @returns {number}
    */
-  public abstract getSessionId(): number;
-
-  /**
-   * @description Returns country.
-   * @returns {ECountry}
-   */
-  public abstract getCountry(): ECountry;
-
-  /**
-   * @description Creates proxy agents with a random country and sessionId.
-   * @param {ICreateProxy} params
-   * @returns {ICreateProxyConfig}
-   */
-  public abstract createAgentsWithRandomCountryAndSessionId(params: ICreateProxy): ICreateProxyConfig;
-
-  /**
-   * @description Creates proxy agents with specific countries and a specific sessionId.
-   * @param {ICreateProxy} params
-   * @returns {ICreateProxyConfig}
-   */
-  public abstract createAgentsSpecificCountryAndSessionsId(params: ICreateProxy): ICreateProxyConfig;
-
-  /**
-   * @description Create proxy agents with a specific country and a random sessionId.
-   * @param {ICreateProxy} params
-   * @returns {ICreateProxyConfig}
-   */
-  public abstract createAgentsSpecificCountriesAndRandomSessionId(params: ICreateProxy): ICreateProxyConfig;
-
-  /**
-   * @description Creates proxy agents with a random country and a specific sessionId
-   * @param {ICreateProxy} params
-   * @returns {ICreateProxyConfig}
-   */
-  public abstract createAgentsRandomCountryAndSpecificSessionId(params: ICreateProxy): ICreateProxyConfig;
+  protected randomNumber(min: number, max: number): number {
+    return Math.floor(min + Math.random() * (max + 1 - min));
+  }
 }
