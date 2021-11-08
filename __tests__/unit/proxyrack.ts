18acd6b58089a403a70de679c2099558e193d34f
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2021-11-08 18:54:59 +0100

    tests(__tests__/unit/proxyrack.ts): test case created

--- __tests__/unit/proxyrack.ts
+++ __tests__/unit/proxyrack.ts
@@ -5,86 +5,17 @@ describe('Proxyrack', () => {
   const proxy = {
     username: 'tictactrip',
     password: 'secret',
-    host: 'megaproxy.rotating.proxyrack.net',
-    port: 222,
   };
-
-  describe('#constructor', () => {
-    it('should set an axios default configuration (User-Agent header)', async () => {
-      const userAgent =
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36';
-      const response1 = {
-        ip: '216.74.105.107',
-        country: 'BE',
-        asn: {
-          asnum: 9009,
-          org_name: 'M247 Ltd',
-        },
-        geo: {
-          city: 'Brussels',
-          region: 'BRU',
-          region_name: 'Brussels Capital',
-          postal_code: '1000',
-          latitude: 50.8336,
-          longitude: 4.3337,
-          tz: 'Europe/Brussels',
-          lum_city: 'brussels',
-          lum_region: 'bru',
-        },
-      };
-
-      const request1Mock = nock('https://lumtest.com')
-        .get('/myip.json')
-        .matchHeader('User-Agent', userAgent)
-        .once()
-        .reply(200, response1);
-
-      const proxyrack: Proxyrack = new Proxyrack({
-        proxy,
-        axiosConfig: {
-          headers: {
-            'User-Agent': userAgent,
-          },
-        },
-      });
-
-      const { data } = await proxyrack.setIp().fetch({
-        method: 'get',
-        baseURL: 'https://lumtest.com',
-        url: '/myip.json',
-      });
-
-      expect(data).toStrictEqual(response1);
-
-      request1Mock.done();
-    });
-  });
-
-  describe('#setIp', () => {
-    let proxyrack: Proxyrack;
-
-    beforeEach(() => {
-      proxyrack = new Proxyrack({ proxy });
-    });
-
-    it('should create an agent', async () => {
-      const agent: Proxyrack = proxyrack.setIp();
-
-      expect(agent).toBeInstanceOf(Proxyrack);
-      expect(agent.axios.defaults.httpsAgent.proxy.host).toBe('megaproxy.rotating.proxyrack.net');
-      expect(agent.axios.defaults.httpsAgent.proxy.port).toBe(222);
-      expect(agent.axios.defaults.httpsAgent.proxy.rejectUnauthorized).toBe(false);
-      expect(agent.axios.defaults.httpsAgent.proxy.auth).toMatch(new RegExp(`${proxy.username}:${proxy.password}`));
-    });
-  });
 
   describe('#strategy', () => {
     describe('CHANGE_IP_EVERY_REQUESTS', () => {
       const proxyrack: Proxyrack = new Proxyrack({
-        proxy,
-        strategy: {
-          mode: EStrategyMode.CHANGE_IP_EVERY_REQUESTS,
+        proxy: {
+          ...proxy,
+          host: 'megaproxy.rotating.proxyrack.net',
+          port: 222,
         },
+        strategy: EStrategyMode.CHANGE_IP_EVERY_REQUESTS,
       });
 
       const response1 = {
@@ -154,6 +85,131 @@ describe('Proxyrack', () => {
 
       describe('https', () => {
         it('should change ip every request', async () => {
+          const request1Mock = nock('https://lumtest.com').get('/myip.json').once().reply(200, response1);
+          const request2Mock = nock('https://lumtest.com').get('/myip.json').once().reply(200, response2);
+
+          const result1 = await proxyrack.fetch({
+            method: 'get',
+            baseURL: 'https://lumtest.com',
+            url: '/myip.json',
+          });
+
+          const result2 = await proxyrack.fetch({
+            method: 'get',
+            baseURL: 'https://lumtest.com',
+            url: '/myip.json',
+          });
+
+          expect(result1.data).toStrictEqual(response1);
+          expect(result2.data).toStrictEqual(response2);
+
+          request1Mock.done();
+          request2Mock.done();
+        });
+      });
+    });
+
+    describe('MANUAL', () => {
+      const proxyrack: Proxyrack = new Proxyrack({
+        proxy: {
+          ...proxy,
+          host: 'mixed.rotating.proxyrack.net',
+          ports: [
+            10000, 10001, 10002, 10003, 10004, 10005, 10006, 10007, 10008, 10009, 10010, 10011, 10012, 10013, 10014,
+            10015, 10016, 10017, 10018, 10019, 10020, 10021, 10022, 10023, 10024, 10025, 10026, 10027, 10028, 10029,
+            10030, 10031, 10032, 10033, 10034, 10035, 10036, 10037, 10038, 10039, 10040, 10041, 10042, 10043, 10044,
+            10045, 10046, 10047, 10048, 10049, 10050, 10051, 10052, 10053, 10054, 10055, 10056, 10057, 10058, 10059,
+            10060, 10061, 10062, 10063, 10064, 10065, 10066, 10067, 10068, 10069, 10070, 10071, 10072, 10073, 10074,
+            10075, 10076, 10077, 10078, 10079, 10080, 10081, 10082, 10083, 10084, 10085, 10086, 10087, 10088, 10089,
+            10090, 10091, 10092, 10093, 10094, 10095, 10096, 10097, 10098, 10099, 10100, 10101, 10102, 10103, 10104,
+            10105, 10106, 10107, 10108, 10109, 10110, 10111, 10112, 10113, 10114, 10115, 10116, 10117, 10118, 10119,
+            10120, 10121, 10122, 10123, 10124, 10125, 10126, 10127, 10128, 10129, 10130, 10131, 10132, 10133, 10134,
+            10135, 10136, 10137, 10138, 10139, 10140, 10141, 10142, 10143, 10144, 10145, 10146, 10147, 10148, 10149,
+            10150, 10151, 10152, 10153, 10154, 10155, 10156, 10157, 10158, 10159, 10160, 10161, 10162, 10163, 10164,
+            10165, 10166, 10167, 10168, 10169, 10170, 10171, 10172, 10173, 10174, 10175, 10176, 10177, 10178, 10179,
+            10180, 10181, 10182, 10183, 10184, 10185, 10186, 10187, 10188, 10189, 10190, 10191, 10192, 10193, 10194,
+            10195, 10196, 10197, 10198, 10199, 10200, 10201, 10202, 10203, 10204, 10205, 10206, 10207, 10208, 10209,
+            10210, 10211, 10212, 10213, 10214, 10215, 10216, 10217, 10218, 10219, 10220, 10221, 10222, 10223, 10224,
+            10225, 10226, 10227, 10228, 10229, 10230, 10231, 10232, 10233, 10234, 10235, 10236, 10237, 10238, 10239,
+            10240, 10241, 10242, 10243, 10244, 10245, 10246, 10247, 10248, 10249, 10250, 10251, 10252, 10253, 10254,
+            10255, 10256, 10257, 10258, 10259, 10260, 10261, 10262, 10263, 10264, 10265, 10266, 10267, 10268, 10269,
+            10270, 10271, 10272, 10273, 10274, 10275, 10276, 10277, 10278, 10279, 10280, 10281, 10282, 10283, 10284,
+            10285, 10286, 10287, 10288, 10289, 10290, 10291, 10292, 10293, 10294, 10295, 10296, 10297, 10298, 10299,
+            10300, 10301, 10302, 10303, 10304, 10305, 10306, 10307, 10308, 10309, 10310, 10311, 10312,
+          ],
+        },
+        strategy: EStrategyMode.MANUAL,
+      });
+
+      const response1 = {
+        ip: '184.174.62.231',
+        country: 'FR',
+        asn: {
+          asnum: 9009,
+          org_name: 'M247 Ltd',
+        },
+        geo: {
+          city: 'Paris',
+          region: 'IDF',
+          region_name: 'Île-de-France',
+          postal_code: '75014',
+          latitude: 48.8579,
+          longitude: 2.3491,
+          tz: 'Europe/Paris',
+          lum_city: 'paris',
+          lum_region: 'idf',
+        },
+      };
+
+      const response2 = {
+        ip: '178.171.89.101',
+        country: 'ES',
+        asn: {
+          asnum: 9009,
+          org_name: 'M247 Ltd',
+        },
+        geo: {
+          city: 'Madrid',
+          region: 'MD',
+          region_name: 'Madrid',
+          postal_code: '28001',
+          latitude: 40.4167,
+          longitude: -3.6838,
+          tz: 'Europe/Madrid',
+          lum_city: 'madrid',
+          lum_region: 'md',
+        },
+      };
+
+      describe('http', () => {
+        it('should change ip on demand', async () => {
+          const request1Mock = nock('http://lumtest.com').get('/myip.json').once().reply(200, response1);
+          const request2Mock = nock('http://lumtest.com').get('/myip.json').once().reply(200, response2);
+
+          const result1 = await proxyrack.fetch({
+            method: 'get',
+            baseURL: 'http://lumtest.com',
+            url: '/myip.json',
+          });
+
+          proxyrack.setIp();
+
+          const result2 = await proxyrack.fetch({
+            method: 'get',
+            baseURL: 'http://lumtest.com',
+            url: '/myip.json',
+          });
+
+          expect(result1.data).toStrictEqual(response1);
+          expect(result2.data).toStrictEqual(response2);
+
+          request1Mock.done();
+          request2Mock.done();
+        });
+      });
+
+      describe('https', () => {
+        it('should change ip on demand', async () => {
           const request1Mock = nock('https://lumtest.com').get('/myip.json').once().reply(200, response1);
           const request2Mock = nock('https://lumtest.com').get('/myip.json').once().reply(200, response2);
 
